6b4644f3fc22fdfee370e8bfca7d12fe123ac7a5
shaanrahman123 <coderraufur@gmail.com>
2023-09-29 01:13:50 +0530

    firstCommit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+document.querySelector("#tab1-tab").addEventListener("click", function () {
+    document.querySelector(".banner-area").style.background = "linear-gradient(90deg, #d4c7b4 37%, #200703 25%)";
+    document.querySelector("#myTabs .active img").style.borderColor = "#200703";
+});
+
+document.querySelector("#tab2-tab").addEventListener("click", function () {
+    document.querySelector(".banner-area").style.background = "linear-gradient(90deg, #d4c7b4 37%, #a55420 25%)";
+    document.querySelector("#myTabs .active img").style.borderColor = "#a55420";
+});
+
+document.querySelector("#tab3-tab").addEventListener("click", function () {
+    document.querySelector(".banner-area").style.background = "linear-gradient(90deg, #d4c7b4 37%, #274829 25%)";
+    document.querySelector("#myTabs .active img").style.borderColor = "#274829";
+
+});
